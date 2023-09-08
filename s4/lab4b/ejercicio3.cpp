a6d5e93ed317b7e256ca20090fe0f9aa8fed6f4d
Paolo Medrano Terán <114374530+paolomedrano04@users.noreply.github.com>
2023-09-08 14:08:24 -0500

    Update ejercicio3.cpp

--- s4/lab4b/ejercicio3.cpp
+++ s4/lab4b/ejercicio3.cpp
@@ -1,23 +1,21 @@
-#include <iostream>
+ #include <iostream>
 
 using namespace std;
 
-template<class...Ts>
+template<typename...Ts>
 float suma_alternada(Ts... args){
     return (args-...);
 }
 
-/*
- args-... = arg1-(arg2-(arg3-(arg4-(arg5))))
-          = arg1-arg2+arg3-arg4+arg5
-
- args-... = arg1-arg2-arg3-arg4-arg5 (INCORRECTO)
-*/
 
 int main(){
     cout << suma_alternada(1) << endl;
-    cout << suma_alternada(3,4) << endl; 
+    // 1
+    cout << suma_alternada(3,4) << endl;
+    // 3-4= -1
     cout << suma_alternada(1,2,3,4,5) << endl;
-    cout << suma_alternada(3,3,2,1,0) << endl;
+    // 1-2+3-4+5=3
+    cout << suma_alternada(4,4,5,6) << endl;
+    // 4op(4op(5op6))
     return 0;
 }
